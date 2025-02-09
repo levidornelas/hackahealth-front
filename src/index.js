ac494427c5a5b0c5660b09ed3c5e5593d3e8aec8
Levi Dornelas <levidornelas2006@gmail.com>
2025-02-09 12:15:45 -0300

    feat: add home

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import EvolucaoPacienteX from './App';
+import HomePage from './App'
 import EvolucaoPacienteY from './paciente-y';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<EvolucaoPacienteY />} />
+      <Route path="/" element={< HomePage />} />
+      <Route path="/dashboard" element={<EvolucaoPacienteY />} />
     </Routes>
   </BrowserRouter>
 );
